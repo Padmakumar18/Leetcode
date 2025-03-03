4cc24ac48db48d2fb506388fca5c34ddf9511059
padmakumar18 <padmakumar41759@gmail.com>
2025-03-03 22:24:49 +0530

    704

---
+++ Binary Search/BinarySearch.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] arr = {1, 3, 5, 7, 9, 11, 15}; 
+        int target = 7;
+
+        int result = solution.search(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
+
+class Solution {
+    public int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
